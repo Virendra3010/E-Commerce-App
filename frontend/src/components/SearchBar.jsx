a873ff0f61f81f0317e81f538a6ef4388d414449
Virendra Patel <virupatel9264@gmail.com>
2025-10-15 15:20:21 +0530

    Refactor SearchBar component to manage visibility based on route

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,12 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets';
 import { ShopContext } from '../context/ShopContext';
+import { useLocation } from 'react-router-dom';
 
 function SearchBar() {
 
     const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
+    const [visible, setvisible] = useState(false);
+    const location = useLocation();
 
-  return showSearch ? (
+    useEffect(() => {
+        if(location.pathname.includes('collection')){
+          setvisible(true);
+        }else{
+          setvisible(false);
+        }
+    }, [location]);
+
+  return showSearch && visible ? (
     <div className='border-t border-b bg-gray-50 text-center'>
         <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
             <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search' />
