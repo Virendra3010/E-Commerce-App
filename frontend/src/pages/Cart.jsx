4941016cbdc6cb2eac3a0d223afeff78e0734e44
Virendra Patel <virupatel9264@gmail.com>
2025-10-20 00:03:14 +0530

    Add getCartAmount function to calculate total cart value and integrate CartTotal component in Cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/cartTotal';
 
 
 function Cart() {
@@ -54,6 +55,11 @@ function Cart() {
             )
           })
         }
+      </div>
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal/>
+        </div>
       </div>
     </div>
   )
