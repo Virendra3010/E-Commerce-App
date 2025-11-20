08f5834959c83bfa61579badcccf846b548f93e2
Virendra Patel <virupatel9264@gmail.com>
2025-11-21 00:10:21 +0530

    Add product controller and integrate product routes into server

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+
+//function for the add the product
+const addProduct = async (req, res) => {
+
+}
+
+//function for list product
+const listProducts = async (req, res) => {
+
+}
+
+//function for the remove product
+const removeProduct = async (req, res) => {
+
+}
+
+//function for the single info
+const singleProduct = async (req, res) => {
+
+}
+
+export {addProduct, removeProduct, listProducts, singleProduct}
