3b183318c8ff15b26d24dcdb292ffb1c350c2825
Virendra Patel <virupatel9264@gmail.com>
2025-12-01 21:47:06 +0530

    feat: enhance Add component with product description, category, price, sizes, and bestseller option

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -28,10 +28,73 @@ const Add = () => {
         </div>
       </div>
 
-      <div>
-        <p>Product Name</p>
+      <div className='w-full'>
+        <p className='mb-2'>Product Name</p>
         <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required />
       </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product Description</p>
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="textarea" placeholder='Write content here' required />
+      </div>
+
+      <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap'>
+
+        <div>
+          <p className='mb-2'>Product category</p>
+          <select className='w-full px-3 py-2'>
+            <option value="Men">Men</option>
+            <option value="Women">Women</option>
+            <option value="Kids">Kids</option>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>Sub category</p>
+          <select className='w-full px-3 py-2'>
+            <option value="Topwear">Topwear</option>
+            <option value="Bottonwear">Bottonwear</option>
+            <option value="Winterwear">Winterwear</option>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>Product Price</p>
+          <input className='w-full px-3 py-2 sm:w-[120px]' type="Number" placeholder='25' />
+        </div>
+      </div>
+
+      <div>
+        <p className='mb-2'>Product Sizes</p>
+        <div className='flex gap-3'>
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex gap-2 mt-2'>
+        <input type="checkbox" id='bestseller' />
+        <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+      </div>
+
+      <button type='submit' className='w-28 py-3 mt-4 bg-black text-white cursor-pointer'>ADD</button>
     </form>
   )
 }
